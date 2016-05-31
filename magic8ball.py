095d1b6b0f72f66887003b9e565b347497f9a236
SetPhasersToStunning <bplosion@gmail.com>
2016-05-31 17:06:02 +0100

    Add files via upload

--- magic8ball.py
+++ magic8ball.py
@@ -7,15 +7,25 @@ answers = [
     'It is certain','It is decidedly so','Without a doubt','Yes, definitely','You may rely on it','As I see it, yes','Most likely','Outlook good','Yes','Signs point to yes','Reply hazy try again','Ask again later','Better not tell you now','Cannot predict now','Concentrate and ask again','Dont count on it','My reply is no','My sources say no','Outlook not so good','Very doubtful',
 ]
 
-print('What is your question? (yes or no answer):')
-question = input(">> ")
+def PlayGame():
+    print('What is your question? (yes or no answer):')
+    question = input(">> ")
 
-match = re.match(r'^[A-Za-z ]*\??$', question) #regex for characters only and a question mark if added with spaces
+    match = re.match(r'^[A-Za-z ]*\??$', question) #regex for characters only and a question mark if added with spaces
 
-if match:
-    print("thinking...")
-    print('Your answer is: %s' %(answers[number]))
-    #    retry = input('Would you like to answer another question?(y/n)')
+    if match:
+        print("thinking...")
+        print('Your answer is: %s' %(answers[number]))
+        PlayAgain()
 
-else:
-    print("Invalid question (characters only) :(")
+    else:
+        print("Invalid question (characters only) :(")
+
+def PlayAgain():
+    playagainanswer = input('Would you like to play again? (y/n)')
+    if playagainanswer == 'y':
+        PlayGame()
+    else:
+        quit()
+
+PlayGame()
